0ce7d95d83552cc0fa7d2c565f18a210f5d162c4
Andrea <agdiiura@users.noreply.github.com>
2020-02-20 14:02:55 +0100

    Update loader.sh

--- mongodb/assets/loader.sh
+++ mongodb/assets/loader.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 apt install subversion -y
 svn checkout https://github.com/huynhsamha/quick-mongo-atlas-datasets/trunk/dump/sample_airbnb
+svn checkout https://github.com/huynhsamha/quick-mongo-atlas-datasets/trunk/dump/sample_mflix
 pip3 install pymongo
 python3 loader.py
