2d43f3c719896e404755bd3a34936f449b4e2356
agdiiura <diyu182@gmail.com>
2020-03-10 08:22:13 +0100

    test for loading

--- mongodb/assets/loader.sh
+++ mongodb/assets/loader.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 apt install subversion -y
-svn checkout https://github.com/agdiiura/enel-mongodb-data/trunk/airbnb
+svn checkout https://github.com/agdiiura/enel-mongodb-data/trunk/ .
 
 pip3 install pymongo
 python3 loader.py
